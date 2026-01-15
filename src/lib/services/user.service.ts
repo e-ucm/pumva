8957ca9764039e723202a09d689b683bd966c79f
Julio SANTILARIO BERTHILIER <jsantilariob@gmail.com>
2026-01-15 11:29:28 +0100

    fix user service and test

--- src/lib/services/user.service.ts
+++ src/lib/services/user.service.ts
@@ -8,6 +8,16 @@ export async function getUsers(): Promise<InstanceType<typeof db.Tables.User>[]>
 }
 
 /**
+ * Get user by its user_id
+ * @param user_id user identifier
+ * @returns specified user
+ */
+export async function getUserById(user_id : number): Promise<InstanceType<typeof db.Tables.User> | null> {
+    const result = await db.Tables.User.findByPk(user_id);
+    return result;
+}
+
+/**
  * Get an user by its username 
  * @param username username of the user
  * @returns the user we're looking 
@@ -27,11 +37,54 @@ export async function createUser(username: string, email: string, role: string):
   return db.Tables.User.create({ username, email, role });
 }
 
+
 /**
- * Delete a user 
- * @param user_id the id of the user to delete
- * @returns the number of user row deleted (normally 1) 
+ * Update BULK users
+ * @param where options to select users to update
+ * @param payload partial user to updates
+ * @returns the number of updated users
  */
-export async function deleteUser(user_id: number): Promise<number>  {
-  return db.Tables.User.destroy({ where : { user_id }});
+export async function updateUsers(where: Partial<InstanceType<typeof db.Tables.User>>, payload : Partial<InstanceType<typeof db.Tables.User>>): Promise<number> {
+  const [affectedRows] = await db.Tables.User.update(payload, { where : where });
+  return affectedRows;
+}
+
+/**
+ * Update ONE user
+ * @param userId user identifier
+ * @param payload partial user to update
+ * @returns the updated user 
+ */
+export async function updateUserById(userId: number, payload: Partial<InstanceType<typeof db.Tables.User>>): Promise<InstanceType<typeof db.Tables.User>> {
+  return db.sequelize.transaction(async (t) => {
+    const user = await db.Tables.User.findByPk(userId, { transaction: t });
+    if (!user) {
+      throw new Error("User not found");
+    }
+    await user.update(payload, { transaction: t });
+    return user;
+  });
+}
+
+/**
+ * Delete user
+ * @param userId user identifier to delete
+ */
+export async function deleteUserById(userId: number): Promise<void> {
+  return db.sequelize.transaction(async (t) => {
+    const user = await db.Tables.User.findByPk(userId, { transaction: t });
+    if (!user) {
+      throw new Error("User not found");
+    }
+    await user.destroy({ transaction: t });
+  });
+}
+
+/**
+ * Delete users
+ * @param where options to select users to delete
+ * @returns the number of users row deleted
+ */
+export async function deleteUsers(where: Partial<InstanceType<typeof db.Tables.User>>): Promise<number> {
+  return db.Tables.User.destroy({ where });
 }
--- src/tests/user/user.service.test.ts
+++ src/tests/user/user.service.test.ts
@@ -1,7 +1,9 @@
-import { createUser, getUserByUsername, getUsers } from "@/lib/services/user.service";
+import { createUser, getUserByUsername, getUsers, updateUsers, updateUserById, deleteUserById, deleteUsers } from "@/lib/services/user.service";
 import { db } from "@/lib/db";
 import { config } from "@/lib/config";
+import { logger } from "@/lib/logger";
 
+var user : InstanceType<typeof db.Tables.User> | null;
 describe("User service", () => {
   beforeAll(async () => {
     try {
@@ -17,14 +19,13 @@ describe("User service", () => {
   });
 
   it("creates a user", async () => {
-    var user = await getUserByUsername("Bob");
-    if(!user) {
-        user = await createUser("Bob", "bob@test.com", "tester");
-    }
+    user = await createUser("Bob", "bob@test.com", "tester");
     expect(user).toBeDefined();
     if(user) {
       expect(user.user_id).toBeDefined();
       expect(user.username).toBe("Bob");
+      expect(user.email).toBe("bob@test.com");
+      expect(user.role).toBe("tester");
     }
   });
 
@@ -32,4 +33,45 @@ describe("User service", () => {
     const users = await getUsers();
     expect(users.length).toBeGreaterThanOrEqual(1);
   });
+
+  it("update user by Id", async () => {
+    user = await updateUserById(user!.user_id, { username: "Tot" });
+    expect(user).toBeDefined();
+    if(user) {
+      expect(user.user_id).toBeDefined();
+      expect(user.username).toBe("Tot");
+      expect(user.email).toBe("bob@test.com");
+      expect(user.role).toBe("tester");
+    }
+  });
+
+  
+  it("update users", async () => {
+    const nb = await updateUsers({ role: "tester" }, { email : "tot@test.com"});
+    expect(nb).toBeDefined();
+    expect(nb).toBe(1);
+    user = await getUserByUsername("Tot");
+    if(user) {
+      expect(user.user_id).toBeDefined();
+      expect(user.username).toBe("Tot");
+      expect(user.email).toBe("tot@test.com");
+      expect(user.role).toBe("tester");
+    }
+  });
+
+    it("delete users", async () => {
+      const nb = await deleteUsers({ role: "tester" });
+      expect(nb).toBeDefined();
+      expect(nb).toBe(1);
+      const users = await getUsers();
+      expect(users.length).toBeGreaterThanOrEqual(1);
+    });
+
+    it("delete user by id", async () => {
+      user = await createUser("Charles", "charles@test.com", "tester");
+      expect(user).toBeDefined();
+      await deleteUserById(user!.user_id);
+      user = await getUserByUsername("Charles");
+      expect(user).toBeNull();
+    });
 });
